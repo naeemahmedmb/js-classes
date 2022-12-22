82d70d49a47023a5f5fb5afd23a12afa034c1c7a
naeemahmedmb <naeemahmedmb@gmail.com>
2022-12-22 10:04:24 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+// var num = 16;
+// document.write("<br/>Result is: "+num *num);
+
+// var num =25;
+// document.write("<br/>Result is: "+num *num);
+//using funcation
+function square(){
+    var num = 16;
+    document.write("<br/>Result is: " + num * num);
+  }
+  square();
+  
