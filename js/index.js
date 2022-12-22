8c8dbe42daf1ac134145e9c0bd3d7482c94a7565
naeemahmedmb <naeemahmedmb@gmail.com>
2022-12-22 10:49:57 +0600

    paramatrized function added

--- js/index.js
+++ js/index.js
@@ -5,9 +5,18 @@
 // var num =25;
 // document.write("<br/>Result is: "+num *num);
 //using funcation
-function square(){
-    var num = 16;
-    document.write("<br/>Result is: " + num * num);
-  }
-  square();
+// function square(){
+//     var num = 16;
+//     document.write("<br/>Result is: " + num * num);
+//   }
+function Mathmatic(num1,num2,num3){
+document.write("<br/> The Addition: " + (num1+num2+num3));
+document.write("<br/> The subtractrion: " + (num3-num2-num1));
+document.write("<br/> The multi: " + (num1*num2*num3));
+document.write("<br/> The division: " + ((num1/num2)/num3));
+}
+let num1 = Number (prompt("Enter a Number"));
+let num2 = Number (prompt("Enter a Number"));
+let num3 = Number (prompt("Enter a Number"));
   
+Mathmatic(num1,num2,num3);
